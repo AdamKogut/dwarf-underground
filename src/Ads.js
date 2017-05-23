429b937ed5af2e7382e3f299dc1187cafa95b35a
AdamKogut <akogut@purdue.edu>
2017-05-23 15:09:19 -0400

    Fix bugs in ads

--- src/Ads.js
+++ src/Ads.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import './Ads.css'
 
 class Ads extends Component{
     render(){
         return (
             <div className="small-12 columns other-articles">
                 <h2>From around the Realm</h2>
-                {this.props.links.map(link => <ArticleLink link={link} />)}
+                {this.props.links.map((link,i) => <ArticleLink link={link} key={i}/>)}
             </div>
         )
     }
